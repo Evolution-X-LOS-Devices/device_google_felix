a9e2cc9f9d2277dbcadeb0d17e7f4ff06e23a9f2
AnierinB <anierin@evolution-x.org>
2024-04-22 13:54:00 -0700

    felix: Initialize for Evolution X 9.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- lineage_felix.mk
+++ lineage_felix.mk
@@ -4,14 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_foldable_book_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/felix/aosp_felix.mk)
 $(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/gs201/evolution_common.mk)
 $(call inherit-product, device/google/felix/device-lineage.mk)
+$(call inherit-product, device/google/felix/device-evolution.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
